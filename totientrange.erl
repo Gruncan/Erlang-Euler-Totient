1253d24b02c4630b286e274de76c00f0e4022f47
Duncan Jones <dj223@outlook.com>
2024-11-30 20:22:16 +0000

    Started rewrite for chaos monkey

--- totientrange.erl
+++ totientrange.erl
@@ -2,13 +2,40 @@
 -export([hcf/2,
          relprime/2,
          sumTotient/3,
-        eularWorker/1,
+        eularWorker/0,
         euler/1,
-        stop_workers/1,
-        start_workers/2,
-        assign_work/3,
-        collect_results/2
+        start_workers/1,
+        assign_work/4,
+        collect_results/3,
+        workerName/1,
+        workerChaos/2,
+        supervisor/2
         ]).
+
+
+workerName(N) ->
+  list_to_atom( "worker" ++ integer_to_list(N)).
+
+
+workerChaos(NVictims, NWorkers) ->
+  lists:map(
+    fun( _ ) ->
+      timer:sleep(500), %% Sleep for .5s
+      %% Choose a random victim
+      WorkerNum = rand:uniform(NWorkers),
+      io:format("workerChaos killing ~p~n", [workerName(WorkerNum)]),
+
+      WorkerPid = whereis(workerName(WorkerNum)),
+      if
+      WorkerPid == undefined ->
+        io:format("workerChaos already dead: ~p~n", [workerName(WorkerNum)]);
+      true -> %% Kill Kill Kill
+        exit(whereis(workerName(WorkerNum)),chaos)
+      end
+    end,
+  lists:seq( 1, NVictims ) ).
+
+
 
 
 hcf(X,0) -> X;
@@ -21,13 +48,10 @@ euler(N) ->
     length(lists:filter(RelprimeN, (lists:seq(1,N)))).
 
 
-eularWorker(MasterId) ->
-  % io:format("MasterId: ~p ~n",[MasterId]),
+eularWorker() ->
   receive
-    {work, WorkList} ->
-      MasterId ! {done, self(), lists:sum(lists:map(fun euler/1, WorkList))};
-    stop ->
-      done
+    {work, CollectorID, WorkList} ->
+      CollectorID ! {done, self(), lists:sum(lists:map(fun euler/1, WorkList))}
   end.
 
 
@@ -47,46 +71,72 @@ printElapsed(S,US) ->
     io:format("Time taken in Secs, MicroSecs ~p ~p~n",[S3-S,US3-US]).
 
 
-stop_workers([]) -> ok;
-stop_workers([Worker | RemWorkers]) ->
-  Worker ! stop,
-  stop_workers(RemWorkers).
+
+start_workers(0) -> [];
+start_workers(Workers) ->
+    [spawn_link(totientrange, eularWorker, []) | start_workers(Workers - 1)].
 
 
-start_workers(0, _) -> [];
-start_workers(Workers, MasterId) ->
-    [spawn(totientrange, eularWorker, [MasterId]) | start_workers(Workers - 1, MasterId)].
+supervisor([], _) -> stop;
+supervisor(Workers, Collector) ->
+  process_flag(trap_exit, true),
+  io:format("Supervising ~p workers ~n", [length(Workers)]),
+  receive
+    {'EXIT', ProcessName, normal} ->
+      % Process is finished we can remove it from the list.
+      supervisor(lists:delete(ProcessName, Workers), Collector);
+    {'EXIT', ProcessName, _} ->
+      % We register under the same name, so do not need to update the list.
+      register(ProcessName, spawn_link(totientrange, eularWorker, [])),
+      supervisor(Workers, Collector);
+    finished ->
+      io:format("Supervision complete~n")
+  end.
+  
 
 
-assign_work([], _, _) -> ok;
-assign_work(Work, [Worker], _) ->
-  Worker ! {work, Work};
 
-assign_work(Work, [Worker | Workers], Chunk ) ->
+assign_work([], _, _, _) -> ok;
+assign_work(Work, [Worker], CollectorID, _) ->
+  Worker ! {work, CollectorID, Work};
+
+assign_work(Work, [Worker | Workers], CollectorID, Chunk ) ->
   {AsgWork, RemWork} = lists:split(Chunk, Work),
 
-  Worker ! {work, AsgWork},
-  assign_work(RemWork, Workers, Chunk).
+  Worker ! {work, CollectorID, AsgWork},
+  assign_work(RemWork, Workers, CollectorID, Chunk).
 
-collect_results([], FinalResult) ->
-  FinalResult;
-collect_results([Worker | Workers], FinalResult) ->
+
+collect_results(MasterId, [], FinalResult) ->
+  MasterId ! {done, FinalResult};
+collect_results(MasterId, [Worker | Workers], FinalResult) ->
   receive
     {done, Worker, Result} ->
-      collect_results(Workers, FinalResult + Result)
+      collect_results(MasterId, Workers, FinalResult + Result)
   end.
 
 
 %%sumTotient lower upper = sum (map euler [lower, lower+1 .. upper])
 sumTotient(Lower, Upper, MaxWorkers) ->
     {_, S, US} = os:timestamp(),
-    Workers = start_workers(MaxWorkers, self()),
+    
+
+    Workers = start_workers(MaxWorkers),
+    io:format("Workers ~p~n", [Workers]),
+    Collector = spawn(totientrange, collect_results, [self(), Workers, 0]),
+
     Work = lists:seq(Lower, Upper),
 
-    assign_work(Work, Workers, length(Work) div length(Workers)),
-    stop_workers(Workers),
+    Chunk = length(Work) div length(Workers),
 
-    Res = collect_results(Workers, 0),
+    assign_work(Work, Workers, Collector, Chunk),
 
-    io:format("Sum of totients: ~p~n", [Res]),
-    printElapsed(S,US).
+    supervisor(Workers, Collector),
+
+    receive
+      {done, Res} ->
+        io:format("Sum of totients: ~p~n", [Res]),
+        printElapsed(S,US)
+    end.
+
+    
