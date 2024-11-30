a1ba6e9d7712bbebae2dfd47dffaab799570be8c
Duncan Jones <dj223@outlook.com>
2024-11-30 15:27:45 +0000

    Parallel calculation now works

--- totientrange.erl
+++ totientrange.erl
@@ -3,32 +3,34 @@
          relprime/2,
          sumTotient/3,
         eularWorker/1,
+        euler/1,
         stop_workers/1,
         start_workers/2,
-        assign_work/3
+        assign_work/3,
+        collect_results/2
         ]).
 
 
-%% hcf x 0 = x
-%% hcf x y = hcf y (rem x y)
 hcf(X,0) -> X;
 hcf(X,Y) -> hcf(Y,X rem Y).
 
-%% relprime x y = hcf x y == 1
 relprime(X,Y) -> hcf(X,Y) == 1.
 
+euler(N) ->
+    RelprimeN = fun(Y) -> relprime(N,Y) end,
+    length(lists:filter(RelprimeN, (lists:seq(1,N)))).
 
-eularWorker(masterId) ->
+
+eularWorker(MasterId) ->
+  % io:format("MasterId: ~p ~n",[MasterId]),
   receive
-    {work, N} ->
-      RelprimeN = fun(Y) -> relprime(N,Y) end,
-      masterId ! {done, self/0(), length/1(lists:filter/2(RelprimeN,(lists:seq/2(1,N))))};
+    {work, WorkList} ->
+      MasterId ! {done, self(), lists:sum(lists:map(fun euler/1, WorkList))};
     stop ->
       done
   end.
 
 
-%%euler n = length (filter (relprime n) (mkList n))
 
 %% Take completion timestamp, and print elapsed time
 printElapsed(S,US) ->
@@ -53,25 +55,38 @@ stop_workers([Worker | RemWorkers]) ->
 
 start_workers(0, _) -> [];
 start_workers(Workers, MasterId) ->
-    [spawn/3(totientrange, eularWorker, [MasterId]) | start_workers(Workers - 1)].
+    [spawn(totientrange, eularWorker, [MasterId]) | start_workers(Workers - 1, MasterId)].
 
 
 assign_work([], _, _) -> ok;
-assign_work([Work | RemWork], Workers, Counter) ->
-  Index = Counter rem length/1(Workers),
-  lists:nth/2(Index, Workers) ! {work, self/1(), Work},
-  assign_work(RemWork, Workers, Counter+1).
+assign_work(Work, [Worker], _) ->
+  Worker ! {work, Work};
 
+assign_work(Work, [Worker | Workers], Chunk ) ->
+  {AsgWork, RemWork} = lists:split(Chunk, Work),
 
-collect_results
+  Worker ! {work, AsgWork},
+  assign_work(RemWork, Workers, Chunk).
+
+collect_results([], FinalResult) ->
+  FinalResult;
+collect_results([Worker | Workers], FinalResult) ->
+  receive
+    {done, Worker, Result} ->
+      collect_results(Workers, FinalResult + Result)
+  end.
+
 
 %%sumTotient lower upper = sum (map euler [lower, lower+1 .. upper])
-sumTotient(Lower,Upper, MaxWorkers) ->
+sumTotient(Lower, Upper, MaxWorkers) ->
     {_, S, US} = os:timestamp(),
-    Workers = start_workers(MaxWorkers, self/0()),
-    assign_work(lists:seq/2(Lower, Upper), Workers, 0),
+    Workers = start_workers(MaxWorkers, self()),
+    Work = lists:seq(Lower, Upper),
+
+    assign_work(Work, Workers, length(Work) div length(Workers)),
     stop_workers(Workers),
 
+    Res = collect_results(Workers, 0),
 
     io:format("Sum of totients: ~p~n", [Res]),
     printElapsed(S,US).
